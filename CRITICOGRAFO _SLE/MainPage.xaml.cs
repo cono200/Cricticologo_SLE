9d84c612edab75f6a7ab9e6ea4d1e24492ddc13e
Cesar-Andres-R <bocchi.the.rock.010@gmail.com>
2024-01-10 09:15:13 -0700

    terminar la funcionalidad D=

--- CRITICOGRAFO _SLE/MainPage.xaml.cs
+++ CRITICOGRAFO _SLE/MainPage.xaml.cs
@@ -14,5 +14,90 @@ namespace CRITICOGRAFO__SLE
         {
             InitializeComponent();
         }
+        string opcion;
+        string opcionAlta;
+        string opcionFea;
+        string opcionLista;
+        string opcionExtravagante;
+        string opcionGrande;
+        string opcionRaro;
+        bool genero;
+        string generoLetras;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            var nombre = Nombre.Text;
+            string resultado = "";
+
+
+            if (mujer.IsChecked)
+            {
+                if (Alto.IsChecked)
+                {
+                    opcionAlta += "alta";
+
+                }
+                if (Feo.IsChecked)
+                {
+                    opcionFea += "fea";
+                }
+                if (Listo.IsChecked)
+                {
+                    opcionLista += "lista";
+                }
+                if (Extravagante.IsChecked)
+                {
+                    opcionExtravagante += "extravagante";
+                }
+                if (Grande.IsChecked)
+                {
+                    opcionGrande += "grande";
+                }
+                if (Raro.IsChecked)
+                {
+                    opcionRaro += "rara";
+                }
+                resultado = nombre + " es "   + opcionAlta +  "," + opcionFea + "," +  opcionLista + "," + opcionExtravagante + ","  + opcionGrande + "," + opcionRaro;
+            } 
+            else
+            {
+                generoLetras = "hombre";
+
+
+                if (Alto.IsChecked)
+                {
+                    opcion += "alto";
+
+                }
+                if (Feo.IsChecked)
+                {
+                    opcion += "feo";
+                }
+                if (Listo.IsChecked)
+                {
+                    opcion += "listo";
+                }
+                if (Extravagante.IsChecked)
+                {
+                    opcion += "extravagante";
+                }
+                if (Grande.IsChecked)
+                {
+                    opcion += "grande";
+                }
+                if (Raro.IsChecked)
+                {
+                    opcion += "raro";
+                }
+                
+            }
+            Resultado.Text = resultado.ToString();
+            
+           
+
+
+
+
+        }
     }
 }
