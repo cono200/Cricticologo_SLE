6f0fff02ea58fce368e398b057caaa82b5dd7114
cono200 <tatattian04@gmail.com>
2024-01-14 16:55:03 -0700

    terminado siuuuuuuuuuuuuuuuuuuuuuuuuuuuuu

--- CRITICOGRAFO _SLE/MainPage.xaml.cs
+++ CRITICOGRAFO _SLE/MainPage.xaml.cs
@@ -25,7 +25,7 @@ namespace CRITICOGRAFO__SLE
         string generoLetras;
         private void Button_Clicked(object sender, EventArgs e)
         {
-           
+            var nombre = Nombre.Text;
 
             if (mujer.IsChecked)
             {
@@ -69,79 +69,69 @@ namespace CRITICOGRAFO__SLE
                     {
                         resultado += opciones[i] + ", ";
                     }
-                    resultado += "y " + opciones[contador - 1];
+                    resultado += "y " + opciones[contador - 1] + ".";
                 }
-                else if (contador == 1)
+                else 
                 {
-                    resultado = opciones[0];
+                    resultado = opciones[0] + ".";
                 }
 
-                Resultado.Text = Nombre + " es " +  resultado;
+                Resultado.Text =  nombre + " es " +  resultado;
             }
 
 
 
+            else
+            {
+                string[] opciones = new string[6];
+                int contador = 0;
+                if (Alto.IsChecked)
+                {
+                    opciones[contador] = "alto";
+                    contador++;
+                }
+                if (Feo.IsChecked)
+                {
+                    opciones[contador] = "feo";
+                    contador++;
+                }
+                if (Listo.IsChecked)
+                {
+                    opciones[contador] = "listo";
+                    contador++;
+                }
+                if (Extravagante.IsChecked)
+                {
+                    opciones[contador] = "extravagante";
+                    contador++;
+                }
+                if (Grande.IsChecked)
+                {
+                    opciones[contador] = "grande";
+                    contador++;
+                }
+                if (Raro.IsChecked)
+                {
+                    opciones[contador] = "raro";
+                    contador++;
+                }
 
-            //var nombre = Nombre.Text;
-            //// string resultado = "";
-            //// int resultado = 0;
-            //string[] opciones = new string[6];
-            //int contador = 0;
+                string resultado = "";
+                if (contador > 1)
+                {
+                    for (int i = 0; i < contador - 1; i++)
+                    {
+                        resultado += opciones[i] + ", ";
+                    }
+                    resultado += "y " + opciones[contador - 1] + ".";
+                }
+                else //if (contador == 1)
+                {
+                    resultado = opciones[0] + ".";
+                }
 
-            //if (mujer.IsChecked)
-            //{
-            //    if (Alto.IsChecked)
-            //    {
-            //        opciones[contador] = "alta";
-            //        contador++;
-            //    }
-            //    if (Feo.IsChecked)
-            //    {
-            //        opciones[contador] = "fea";
-            //        contador++;
-            //    }
-            //    if (Listo.IsChecked)
-            //    {
-            //        opciones[contador] = "lista";
-            //        contador++;
-            //    }
-            //    if (Extravagante.IsChecked)
-            //    {
-            //        opciones[contador] = "extravagante";
-            //        contador++;
-            //    }
-            //    if (Grande.IsChecked)
-            //    {
-            //        opciones[contador] = "grande";
-            //        contador++;
-            //    }
-            //    if (Raro.IsChecked)
-            //    {
-            //        opciones[contador] = "rara";
-            //        contador++;
-            //    }
-
-            //    string resultado = "";
-            //    if (contador > 1)
-            //    {
-            //        resultado = nombre + " es " + string.Join(", ", opciones.Take(contador - 1)) + " y " + opciones[contador - 1];
-            //    }
-            //    else if (contador == 1)
-            //    {
-            //        resultado = opciones[0];
-            //    }
-
-            //    Resultado.Text = resultado;
-            //}
-            //else
-            //{
-
-            //}
-
-
-
-
-
+                Resultado.Text = nombre + " es " + resultado;
+            }
 
         }
     }
